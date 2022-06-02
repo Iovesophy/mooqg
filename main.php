6a68ecedbdf70f056714aa9fba75df643a3c4a25
k_yuda <k_yuda@timedia.co.jp>
2022-06-02 22:39:33 +0900

    ADD: help: message

---
+++ main.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env php
+<?php
+/**
+ * mooqg - Moodle quiz generator
+ *
+ * @copyright  2022 Iovesophy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+echo "   _____
+  /____/-.---_  .---.  .---.  .---.  .---.
+  | |  _   _  |/  _  \/  _  \/  _  |/  _  \
+  * | | | | | || |_| || |_| || |_| || |_| |
+    |_| |_| |_|\_____/\_____/\___  |\.__  |
+                                 | || |_| |
+                                 ._. \___/
+
+mooqg - MOOdle + Quiz + Generator
+
+Usage:  mooqg [OPTIONS]
+
+Options:
+  -i, --input_file string
+  -t, --input_type string
+  -o, --output_file string
+  -f, --output_format string";
+
+?>
